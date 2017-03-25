cb839e9a05248ca146e55cb7a00c759ba6ed0e65
gftools <develop.gftools@gmail.com>
2017-03-25 21:22:42 +0900

    Remove debug snippets

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
 
-
-
-
-
-
-
-// $a = password_hash('password', PASSWORD_BCRYPT);
-
-
-// print $a;
-
-
-
 $app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
     echo "Hello, " . $name;
