275c15826f00b598dc74269282727f0c0635e30e
gftools <gftools@users.noreply.github.com>
2017-03-26 15:23:07 +0900

    bower components added

--- application/views/user_registration.php
+++ application/views/user_registration.php
@@ -10,23 +10,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 
-<?php include('navbar.html'); ?>
-
-
 <div class="container-fluid">
 	<h1>Hello My Application</h1>
 </div>
 
-<div id="app" class="container">
-	<h2>{{ message }}</h2>
-</div>
 
-
-<div id="resultset">
-<?php foreach ($resultset as $result): ?>
-<?php print_r($result) ?>
-<?php endforeach; ?>
-</div>
 
 
 
@@ -38,16 +26,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 <?php include('scripts.html'); ?>
-
-<script>
-var app = new Vue({
-	el: '#app',
-	data: {
-		message: 'Hello Vue!'
-	}
-});
-</script>
-
-
 </body>
 </html>
